3386eca375e65e862f8375f5a6513035e48687b2
divyani <ikrakizoi2607@gmail.com>
2026-01-01 23:08:06 +0530

    Refactor and update main class for request handling

--- Basic Programming/src/Alex and Requests.java
+++ Basic Programming/src/Alex and Requests.java
@@ -1,3 +1,4 @@
+//Partially accepted
 import java.io.*;
 import java.util.*;
 
@@ -43,5 +44,45 @@ class TestClass {
         }
 
         System.out.print(sb.toString());
+    }
+}
+
+//accepted 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        assert n >= 1 && n <= 100;
+
+        int q = sc.nextInt();
+        assert q >= 1 && q <= 10000;
+
+        int[] cur = new int[105];
+
+        while (q-- > 0) {
+            int x = sc.nextInt();
+            assert x >= 1 && x <= 100000;
+
+            int val = Math.min(x, n);
+            boolean found = false;
+
+            for (int i = val; i >= 1; i--) {
+                if (cur[i] < x) {
+                    cur[i] = x;
+                    System.out.println("YES");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
     }
 }
