1868b05628794206adba7adb24ae59aa80db6bc1
divyani <ikrakizoi2607@gmail.com>
2026-04-04 00:12:01 +0530

    Add Subtree Query implementation with segment tree

---
+++ Data Structures/src/hard/Subtree Query.swift
@@ -0,0 +1,191 @@
+import Foundation
+
+let N = 100005
+
+var graph = Array(repeating: [Int](), count: N)
+var a = Array(repeating: 0, count: N)
+var pos = Array(repeating: 0, count: N)
+var sub = Array(repeating: 0, count: N)
+var order = [Int]()
+
+// -------- SEGMENT TREE --------
+class SegTree {
+    var n: Int
+    var tree: [Int]
+    var lazy: [[Int]]   // [flip, ?, assignFlag]
+
+    init(_ arr: [Int]) {
+        self.n = arr.count
+        self.tree = Array(repeating: 0, count: 4 * n)
+        self.lazy = Array(repeating: [0, 0, 0], count: 4 * n)
+        build(1, 0, n - 1, arr)
+    }
+
+    func build(_ node: Int, _ start: Int, _ end: Int, _ arr: [Int]) {
+        if start == end {
+            tree[node] = arr[start]
+        } else {
+            let mid = (start + end) / 2
+            build(node * 2, start, mid, arr)
+            build(node * 2 + 1, mid + 1, end, arr)
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
+        }
+    }
+
+    func push(_ node: Int, _ start: Int, _ end: Int) {
+        if lazy[node][2] == 1 {
+            tree[node] = (end - start + 1) * lazy[node][1]
+        }
+
+        if lazy[node][0] == 1 {
+            tree[node] = (end - start + 1) - tree[node]
+        }
+
+        if start != end {
+            if lazy[node][2] == 1 {
+                lazy[node * 2] = lazy[node]
+                lazy[node * 2 + 1] = lazy[node]
+            } else {
+                lazy[node * 2][0] ^= lazy[node][0]
+                lazy[node * 2 + 1][0] ^= lazy[node][0]
+            }
+        }
+
+        lazy[node][0] = 0
+        lazy[node][2] = 0
+    }
+
+    func flipRange(_ node: Int, _ start: Int, _ end: Int, _ l: Int, _ r: Int) {
+        if start > r || end < l { return }
+
+        push(node, start, end)
+
+        if start >= l && end <= r {
+            lazy[node][0] = 1
+            push(node, start, end)
+            return
+        }
+
+        let mid = (start + end) / 2
+        flipRange(node * 2, start, mid, l, r)
+        flipRange(node * 2 + 1, mid + 1, end, l, r)
+
+        tree[node] = tree[node * 2] + tree[node * 2 + 1]
+    }
+
+    func getSum(_ node: Int, _ start: Int, _ end: Int, _ l: Int, _ r: Int) -> Int {
+        if start > r || end < l { return 0 }
+
+        push(node, start, end)
+
+        if start >= l && end <= r {
+            return tree[node]
+        }
+
+        let mid = (start + end) / 2
+        return getSum(node * 2, start, mid, l, r)
+             + getSum(node * 2 + 1, mid + 1, end, l, r)
+    }
+
+    func flipRange(_ l: Int, _ r: Int) {
+        flipRange(1, 0, n - 1, l, r)
+    }
+
+    func getSum(_ l: Int, _ r: Int) -> Int {
+        return getSum(1, 0, n - 1, l, r)
+    }
+}
+
+// -------- DFS --------
+func dfs(_ u: Int, _ parent: Int) {
+    sub[u] = 1
+    order.append(u)
+
+    for v in graph[u] {
+        if v != parent {
+            dfs(v, u)
+            sub[u] += sub[v]
+        }
+    }
+}
+
+// -------- MAIN SOLUTION --------
+func solve() {
+    let n = Int(readLine()!)!
+
+    for i in 1...n {
+        graph[i] = []
+    }
+
+    for _ in 0..<(n - 1) {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        let x = input[0], y = input[1]
+        graph[x].append(y)
+        graph[y].append(x)
+    }
+
+    let values = readLine()!.split(separator: " ").map { Int($0)! }
+    for i in 1...n {
+        a[i] = values[i - 1]
+    }
+
+    dfs(1, 0)
+
+    for i in 0..<n {
+        pos[order[i]] = i
+    }
+
+    // 20 segment trees
+    var seg = [SegTree]()
+
+    for i in 0..<20 {
+        var arr = Array(repeating: 0, count: n)
+
+        for j in 0..<n {
+            if (a[order[j]] & (1 << i)) != 0 {
+                arr[j] = 1
+            }
+        }
+
+        seg.append(SegTree(arr))
+    }
+
+    let q = Int(readLine()!)!
+
+    for _ in 0..<q {
+        let parts = readLine()!.split(separator: " ").map { Int($0)! }
+        let t = parts[0]
+
+        if t == 1 {
+            let node = parts[1]
+
+            let l = pos[node]
+            let r = l + sub[node] - 1
+
+            var res: Int64 = 0
+
+            for i in 0..<20 {
+                let sum = seg[i].getSum(l, r)
+                res += Int64(sum) * (1 << i)
+            }
+
+            print(res)
+
+        } else {
+            let node = parts[1]
+            let x = parts[2]
+
+            let l = pos[node]
+            let r = l + sub[node] - 1
+
+            for i in 0..<20 {
+                if (x & (1 << i)) != 0 {
+                    seg[i].flipRange(l, r)
+                }
+            }
+        }
+    }
+}
+
+// -------- RUN --------
+solve()
